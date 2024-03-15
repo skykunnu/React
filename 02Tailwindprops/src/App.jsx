58128ddf1fb375c3576706d1f2c14089d4e9fa1d
Shikhar Khandelwal <skykunnu@gmail.com>
2024-03-15 23:10:22 +0530

    init

--- 02Tailwindprops/src/App.jsx
+++ 02Tailwindprops/src/App.jsx
@@ -7,7 +7,28 @@ function App() {
   return (
     <>
     {/* Here className is a reserve keyword used to write tailwind css.*/}
-     <h1 className='text-3xl font bold underline'>Tailwind Test</h1> 
+     <h1 className='bg-green-400 text-black p-2 rounded-xl mb-4'>Tailwind Test</h1>
+
+     <div class="relative h-[400px] w-[300px] rounded-md">
+  <img
+    src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+    alt="AirMax Pro"
+    class="z-0 h-full w-full rounded-md object-cover"
+  />
+  <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+  <div class="absolute bottom-4 left-4 text-left">
+    <h1 class="text-lg font-semibold text-white">Shakira</h1>
+    <p class="mt-2 text-sm text-gray-300">
+      The Pop Singer of 20th Century. 
+    </p>
+    <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+      View Profile →
+    </button>
+  </div>
+</div>
+   
+    
+    
     </>
   )
 }
