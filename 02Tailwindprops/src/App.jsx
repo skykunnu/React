61f487f47f49937afc33b7c3b215c6ca6d383b3c
Shikhar Khandelwal <skykunnu@gmail.com>
2024-03-17 11:04:08 +0530

    init

--- 02Tailwindprops/src/App.jsx
+++ 02Tailwindprops/src/App.jsx
@@ -3,12 +3,12 @@ import './App.css'
 import Card from './components/card'
 function App() {
   
-
+ 
   return (
     <>
     {/* Here className is a reserve keyword used to write tailwind css.*/}
      <h1 className='bg-green-400 text-black p-2 rounded-xl mb-4'>Tailwind Test</h1>
-    <Card /> <br />
+    <Card/> <br />
     <Card />
     </>
   )
