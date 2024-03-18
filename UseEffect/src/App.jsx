96362a4c006d2fd34f3864dcd007acabcdda6440
Shikhar Khandelwal <skykunnu@gmail.com>
2024-03-18 23:10:48 +0530

    init

--- UseEffect/src/App.jsx
+++ UseEffect/src/App.jsx
@@ -2,16 +2,20 @@ import React,{useEffect} from 'react'
 
 import './App.css'
 import MyComponent from './MyComp';
+import { useState } from 'react';
 
 
 function App() {
+
+const [isVisible,setVisible]=useState(true);
 
 useEffect(()=>{
   console.log("App component is Mounting...");
 },[])
   return (
     <>
-     <MyComponent />
+     {isVisible ? <MyComponent />:<></>}
+     <button onClick={()=>setVisible(!isVisible)}>Toggle</button>
     </>
   )
 }
