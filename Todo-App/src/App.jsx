bb37ef54f453dcc4fc18ee85c8a28aeedc0ec1ee
Shikhar Khandelwal <skykunnu@gmail.com>
2024-03-18 15:00:58 +0530

    init

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Header from "./Components/Header";
+import Header from "./Components/Header.jsx";
 import TodoItem from "./Components/TodoItem";
 import Button from "./Components/Button";
 import "./App.css";
@@ -8,12 +8,13 @@ import "./App.css";
 function App() {
 return (
     <div className="todo-container">
-        <Header />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        <Header title="Todo" /> 
+         {/* below we have passed the text prop in form of attributes. */}
+        <TodoItem text="Eat" completed={true}/>  
+        <TodoItem text="Code"/>
+        <TodoItem  text="Play"/>
+        <TodoItem  text="Sleep"/>
+        <TodoItem text="Repeat"/>
         <Button />
     </div>
 )
--- Todo-App/src/Components/TodoItem.jsx
+++ Todo-App/src/Components/TodoItem.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 
-const TodoItem=()=>{
+const TodoItem=(props)=>{ // here we have defined props 
 return (
     
     <li className="todo-item">
         <span>
-        <input type="checkbox" />
-    <span className="todo-item-text">Eat</span>
+            {/* Making input a conditional rendering */}
+    {props.completed ? <></> : <input type="checkbox" />}
+    
+    <span className="todo-item-text">{props.text}</span> {/* here we are rendering text with the help of props and as it is a dynamic value, it is wrapped under curly braces.*/}
         </span>
     <p>...</p>
     </li>
