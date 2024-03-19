c62ebcf1025b3f88702e2ec3beec012e5a0b7ee3
Shikhar Khandelwal <skykunnu@gmail.com>
2024-03-19 17:36:51 +0530

    delete

--- Tic-Tae-Toe/src/App.jsx
+++ Tic-Tae-Toe/src/App.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
-
-import './App.css'
+import Board from './Tic Tac Toe/Board';
+import './App.css';
 
 function App() {
-
-
   return (
     <>
-     
+     <Board />
     </>
   )
 }
 
-export default App
+export default App;
