272f7f05144704204fa19e4593366d43bf67f730
Shikhar Khandelwal <skykunnu@gmail.com>
2024-03-19 17:56:26 +0530

    init

---
+++ Tic-Tae-Toe/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Board from './tic-tae-toe/Board';
+function App() {
+  return (
+    <div className="App">
+     <Board />
+    </div>
+  );
+}
+
+export default App;
---
+++ Tic-Tae-Toe/src/tic-tae-toe/Square.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Square=()=>{
+    return (
+        <div className="sqaure">
+            <h5>X</h5>
+        </div>
+    )
+}
+
+export default Square;
