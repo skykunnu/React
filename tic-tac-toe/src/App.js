6099863d70941229d2749bd7cf908b8ac1691b35
Shikhar Khandelwal <skykunnu@gmail.com>
2024-03-20 13:52:14 +0530

    init

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Board from './tic-tac-toe/Board';
+function App() {
+  return (
+    <>
+    <Board />
+    </>
+  );
+}
+
+export default App;
