6b43804ec42a61b037a419365079dcdb5c8cd4ef
Shikhar Khandelwal <skykunnu@gmail.com>
2024-06-24 21:28:35 +0530

    delete

--- 01basicVite/src/chai.jsx
+++
@@ -1,7 +0,0 @@
-function Coffee(){
-    return(
-        <h3>Coffee is ready</h3>
-    )
-}
- 
-export default Coffee 
--- Todo-App/src/Components/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Header=(props)=>{
-return <h1 className='todo-header'>{props.title}</h1>
-}
-
-export default Header;
