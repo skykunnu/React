eaef4ed2ee2b24ccad659096ce10db36adca4308
Shikhar Khandelwal <skykunnu@gmail.com>
2024-06-25 18:50:15 +0530

    new

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,14 +1,22 @@
+
+// Below is the custom component
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+       <Header></Header> {/*or <Header /> (self closing) --> custom components */}
       <main>
         <h2>Time to get started!</h2>
       </main>
